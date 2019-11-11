f009d3269d3a0c56634f9c2a785a613f29ff3244
FranciscoGustavo <hidalgo_fco520@outlook.com>
2019-11-11 11:59:26 -0600

    add controller of the products

---
+++ controllers/ProductsController.js
@@ -0,0 +1,80 @@
+const ProductsService = require('../services/ProductsService');
+
+class ProductsController {
+    constructor() {
+        this.productsService = new ProductsService();
+        this.index = this.index.bind(this);
+        this.create = this.create.bind(this);
+        this.show = this.show.bind(this);
+        this.update = this.update.bind(this);
+        this.delete = this.delete.bind(this);
+    }
+
+    async index(req, res, next) {
+        const { tags } = req.query;
+        try {
+            const products = await this.productsService.getProducts({ tags });
+            res.status(200).json({
+                data: products,
+                message: 'products listed'
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async create(req, res, next) {
+        const { body: product } = req;
+        try {
+            const createdProductId = await this.productsService.createProduct({ product });
+            res.status(201).json({
+                data: createdProductId,
+                message: 'product created',
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async show(req, res, next) {
+        const { id: productId } = req.params;
+        try {
+            const product = await this.productsService.getProduct({ productId });
+            res.status(200).json({
+                data: product,
+                message: 'product retrieved',
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async update(req, res, next) {
+        const { id: productId } = req.params;
+        const { body: product } = req;
+        try {
+            const updatedProductId = await this.productsService.updateProduct({ productId, product });
+            res.status(200).json({
+                data: updatedProductId,
+                message: 'product updated',
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async delete(req, res, next) {
+        const { id: productId } = req.params;
+        try {
+            const deletedProductId = await this.productsService.deleteProduct({ productId });
+            res.status(200).json({
+                data: deletedProductId,
+                message: 'product deleted',
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = ProductsController;
