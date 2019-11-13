0c094954b2f61562d999b059a538c205f47ea9e5
FranciscoGustavo <hidalgo_fco520@outlook.com>
2019-11-13 13:24:32 -0600

    add two strategies for aunthentication

---
+++ utils/auth/strategies/basic.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const { BasicStrategy } = require('passport-http');
+const boom = require('@hapi/boom');
+const bycript = require('bcrypt');
+
+const UserService = require('.../../../services/UsersService');
+
+passport.use(
+    new BasicStrategy(
+        async function(email, password, cb) {
+            const userService = new UserService();
+            try {
+                const user = await this.userService.getUser({ email });
+                if(!user) {
+                    return cb(boom.unauthorized(), false);
+                }
+                if(!await bycript.compare(password, user.password)) {
+                    return cb(boom.unauthorized(), false);
+                }
+                delete user.password;
+                return cb(null, user);
+            } catch(error) {
+                return cb(error);
+            }
+        }
+
+    )
+);
