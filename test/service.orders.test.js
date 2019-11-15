99d0feec73fb05b33aa99455a2a7f34ac7b862ef
FranciscoGustavo <hidalgo_fco520@outlook.com>
2019-11-14 19:45:14 -0600

    add testing to service of Orders

--- test/service.orders.test.js
+++ test/service.orders.test.js
@@ -1,6 +1,15 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const proxyquire = require('proxyquire');
-const { MongoLibMock, getAllStub, getStub } = require('../utils/mocks/mongoLib');
+const { 
+    MongoLibMock, 
+    getAllStub, 
+    getStub, 
+    createStub, 
+    createManyStub,
+    updateStub,
+    deleteStub,
+    deleteManyStub
+} = require('../utils/mocks/mongoLib');
 const { ordersMock } = require('../utils/mocks/orders');
 
 describe('service - orders', function() {
@@ -18,21 +27,97 @@ describe('service - orders', function() {
 
         it('should return an array of orders', async function() {
             const result = await ordersService.getOrders({});
-            const expected = ordersMock;
+            const expected = ordersMock.orders;
             assert.deepEqual(result, expected);
         });
     });
 
     describe('when getOrder method is called', async function() {
         it('should call the get MongoLib method', async function() {
-            await ordersService.getOrder({ orderId: ordersMock[0]._id });
+            await ordersService.getOrder({ orderId: ordersMock.orders[0]._id });
             assert.strictEqual(getStub.called, true);
         });
 
-        it('should return an object with product', async function() {
-            const result = await ordersService.getOrder({ orderId: ordersMock[0]._id });
-            const expected = ordersMock[0];
+        it('should return an order with products', async function() {
+            const result = await ordersService.getOrder({ orderId: ordersMock.orders[0]._id });
+            const expected = {
+                ...ordersMock.orders[0],
+                products: ordersMock.products
+            };
             assert.deepEqual(result, expected);
         });
     });
-});*/
+
+    describe('when createOrder method is called', async function() {
+        it('should call the create and createMany MongoLib method', async function() {
+            await ordersService.createOrder({ 
+                order: ordersMock.orders[0], 
+                products: ordersMock.products
+            });
+            assert.strictEqual(createStub.called, true);
+            assert.strictEqual(createManyStub.called, true);
+        });
+
+        it('should return an new object with order and its products', async function() {
+            const result = await ordersService.createOrder({ 
+                order: ordersMock.orders[0], 
+                products: ordersMock.products
+            });
+            const productsIds = ordersMock.products.map(({_id}) => _id);
+            const expected = {
+                createdOrderId: ordersMock.orders[0]._id,  
+                createdIdsOrderProducts: productsIds
+            };
+            assert.deepEqual(result, expected);
+        });
+    });
+
+    describe('when updateOrder method is called', async function() {
+        it('should call the update, deleteMany and createMany MongoLib method', async function() {
+            await ordersService.updateOrder({ 
+                orderId: ordersMock.orders[0]._id, 
+                order: ordersMock.orders[0], 
+                products: ordersMock.products 
+            });
+            assert.strictEqual(updateStub.called, true);
+            assert.strictEqual(deleteManyStub.called, true);
+            assert.strictEqual(createManyStub.called, true);
+        });
+
+        it('should return an object with order and its products updated', async function() {
+            const result = await ordersService.updateOrder({ 
+                orderId: ordersMock.orders[0]._id, 
+                order: ordersMock.orders[0], 
+                products: ordersMock.products 
+            });
+            const productsIds = ordersMock.products.map(({_id}) => _id);
+            const expected = { 
+                updatedOrderId: ordersMock.orders[0]._id, 
+                updatedIdsOrderProducts: productsIds, 
+                deletedListProducts: productsIds.length 
+            };
+            assert.deepEqual(result, expected);
+        });
+    });
+
+    describe('when deleteOrder method is called', async function() {
+        it('should call the delete and deleteMany MongoLib method', async function() {
+            await ordersService.deleteOrder({ 
+                orderId: ordersMock.orders[0]._id,
+            });
+            assert.strictEqual(deleteStub.called, true);
+            assert.strictEqual(deleteManyStub.called, true);
+        });
+
+        it('should return an object with orderId and its number of products deleted', async function() {
+            const result = await ordersService.deleteOrder({ 
+                orderId: ordersMock.orders[0]._id,
+            });
+            const expected = { 
+                deletedOrderId: ordersMock.orders[0]._id, 
+                deletedListProducts: ordersMock.products.length 
+            };
+            assert.deepEqual(result, expected);
+        });
+    });
+});
--- utils/mocks/mongoLib.js
+++ utils/mocks/mongoLib.js
@@ -5,14 +5,28 @@ const { ordersMock } = require('./orders');
 const getAllStub = sinon.stub();
 getAllStub.withArgs('products').resolves(productsMock);
 getAllStub.withArgs('orders').resolves(ordersMock.orders);
+getAllStub.withArgs('orderProducts').resolves(ordersMock.products);
 
 const getStub = sinon.stub();
 getStub.withArgs('orders').resolves(ordersMock.orders[0]);
 getStub.withArgs('products').resolves(productsMock[0]);
 
-//const tagQuery = { tags: { $in: ['drama'] }};
+const createStub = sinon.stub()
+createStub.withArgs('products').resolves(productsMock[0]._id);
+createStub.withArgs('orders').resolves(ordersMock.orders[0]._id);
 
-const createStub = sinon.stub().resolves(productsMock[0]._id);
+const createManyStub = sinon.stub()
+createManyStub.withArgs('orderProducts').resolves(ordersMock.products.map(({_id})=> _id));
+
+const updateStub = sinon.stub()
+updateStub.withArgs('orders').resolves(ordersMock.orders[0]._id);
+
+const deleteStub = sinon.stub()
+deleteStub.withArgs('orders').resolves(ordersMock.orders[0]._id)
+
+const deleteManyStub = sinon.stub()
+deleteManyStub.withArgs('orderProducts').resolves(ordersMock.products.length);
+
 
 class MongoLibMock {
     getAll(collection, query) {
@@ -26,11 +40,31 @@ class MongoLibMock {
     create(collection, data) {
         return createStub(collection, data);
     }
+
+    createMany(collection, data) {
+        return createManyStub(collection, data);
+    }
+
+    update(collection, id, data) {
+        return updateStub(collection, data);
+    }
+
+    delete(collection, id) {
+        return deleteStub(collection, id);
+    }
+
+    deleteMany(collection, data) {
+        return deleteManyStub(collection, data);
+    }
 }
 
 module.exports = {
     getAllStub,
     getStub,
     createStub,
+    createManyStub,
+    updateStub,
+    deleteStub,
+    deleteManyStub,
     MongoLibMock
 }
